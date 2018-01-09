f6fb7c0fdb99c90aad4e85fcbfedc337fbdd829a
Joseph Chen <aoxiang.chen@ele.me>
2018-01-09 11:52:08 +0800

    code go public: sync with local repo

---
+++ ntoken.h
@@ -0,0 +1,14 @@
+#ifndef NTOKEN_H
+#define NTOKEN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ntoken_hash(const char* input, int len, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ ntokenmodule.c
@@ -0,0 +1,57 @@
+#include <Python.h>
+
+#include "ntoken.h"
+
+static PyObject *ntoken_getpowhash(PyObject *self, PyObject *args)
+{
+    char *output;
+    PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
+    PyStringObject *input;
+#endif
+    if (!PyArg_ParseTuple(args, "S", &input))
+        return NULL;
+    Py_INCREF(input);
+    output = PyMem_Malloc(32);
+
+#if PY_MAJOR_VERSION >= 3
+    ntoken_hash((char *)PyBytes_AsString((PyObject*) input), (int)PyBytes_Size((PyObject*) input), output);
+#else
+    ntoken_hash((char *)PyString_AsString((PyObject*) input), (int)PyString_Size((PyObject*) input), output);
+#endif
+    Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
+    value = Py_BuildValue("s#", output, 32);
+#endif
+    PyMem_Free(output);
+    return value;
+}
+
+static PyMethodDef nTokenMethods[] = {
+    { "getPoWHash", ntoken_getpowhash, METH_VARARGS, "Returns the proof of work hash using ntoken hash" },
+    { NULL, NULL, 0, NULL }
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef nTokenModule = {
+    PyModuleDef_HEAD_INIT,
+    "ntoken_hash",
+    "...",
+    -1,
+    nTokenMethods
+};
+
+PyMODINIT_FUNC PyInit_ntoken_hash(void) {
+    return PyModule_Create(&nTokenModule);
+}
+
+#else
+
+PyMODINIT_FUNC initntoken_hash(void) {
+    (void) Py_InitModule("ntoken_hash", nTokenMethods);
+}
+#endif
